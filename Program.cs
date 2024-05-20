76782efbbe6c0fcb1de8bc696a71f20afc9a4873
israel-bsi <masterisraelsilva@gmail.com>
2024-05-19 23:08:02 -0300

    Implementado os TODOs solicitados: propriedades e construtor na classe smartphone, e a herança nas classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Spartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789", 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSpartphone Iphone");
+Smartphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 1024);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
